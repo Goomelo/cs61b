5c0e207a63330d554345ef98f2ef818ed811f0b5
Goomelo <1034570130@qq.com>
2021-04-12 17:40:23 +0800

    Add files via upload

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -12,8 +12,8 @@ public class LinkedListDeque<T> {
     }
     private LinkedNode sentinel;
 
-    public LinkedListDeque(T item) {
-        sentinel = new LinkedNode(item,new LinkedNode(null,null,null),new LinkedNode(null,null,null));
+    public LinkedListDeque() {
+        sentinel = new LinkedNode(null,new LinkedNode(null,null,null),new LinkedNode(null,null,null));
         size = 0;
     }
 
